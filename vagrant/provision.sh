42794f2a4cb242b69226412488a122d5be3303b6
Rein Van Oyen <reinvanoyen@gmail.com>
2018-10-15 16:40:39 +0200

    Development fixes
    - composer symlinking hector package
    - vagrant now syncs hector dir
    - removed composer install hash check from provision

--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -90,7 +90,6 @@ function restartApache {
 function installComposer {
 	cd /home/vagrant
 	php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-	php -r "if (hash_file('SHA384', 'composer-setup.php') === '544e09ee996cdf60ece3804abc52599c22b1f40f4323403c44d44fdfdd586475ca9813a858088ffbc1f233e9b180f061') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
 	php composer-setup.php --filename=composer
 	php -r "unlink('composer-setup.php');"
 	cd /home/vagrant/default
